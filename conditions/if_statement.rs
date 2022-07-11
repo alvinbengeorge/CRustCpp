df9a2852e3bf65810edf987f8622b5eb71e6b25b
alvinbengeorge <alvinallen333@gmail.com>
2022-07-11 08:56:12 +0530

    Added basic programs

---
+++ conditions/if_statement.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let m = 5;
+    if m>0 {
+        println!("Postive");
+        if m>10 {
+            print!("Greater than 10");
+        }
+        else {
+            print!("Less than or equal to 10");
+        }
+    }
+}
---
+++ typing/typing.rs
@@ -0,0 +1,7 @@
+fn main()
+{
+    let int32: i32 = 4;
+    let float32: f32 = 4.32;
+    println!("{} -> {}", float32, int32);
+    println!("{}", 4i32)
+}
