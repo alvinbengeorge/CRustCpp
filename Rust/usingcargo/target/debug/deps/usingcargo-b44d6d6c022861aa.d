59ac89b272df768d975cce0d7ca2c06350d10534
Alvin Ben George <alvinallen333@gmail.com>
2023-06-12 19:23:20 +0530

    Build using cargo

---
+++ Rust/usingcargo/target/debug/deps/usingcargo-b44d6d6c022861aa.d
@@ -0,0 +1,5 @@
+/home/alvin/Desktop/CRustCpp/Rust/usingcargo/target/debug/deps/usingcargo-b44d6d6c022861aa: src/main.rs
+
+/home/alvin/Desktop/CRustCpp/Rust/usingcargo/target/debug/deps/usingcargo-b44d6d6c022861aa.d: src/main.rs
+
+src/main.rs:
